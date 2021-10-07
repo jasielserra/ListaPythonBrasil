60048172abc3aecd149f2bebfe3ffdb4c74400d3
jasiel <jasiel_serra@hotmail.com>
2021-10-07 14:18:52 -0300

    Listening Comprehension

--- Geradores.py
+++ Geradores.py
@@ -4,6 +4,18 @@ def lazy_range(n):
         yield i
         i += 1
 
+def natural_numbers():
+    """ retorna 1, 2, 3, ..."""
+    i = 10
+    while i > 0:
+        yield i
+        i -= 1
+
+lazy_even_below_20 = (i for i in lazy_range(20) if i % 2 == 0)
 
 
 print(list(lazy_range(10)))
+print('=' * 30)
+print(list(natural_numbers()))
+print('=' * 30)
+print(list(lazy_even_below_20))
