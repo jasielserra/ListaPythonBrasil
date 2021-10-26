2d9d0e180f72ea9a876b04b5049720e1df4af96c
jasiel <jasiel_serra@hotmail.com>
2021-10-26 16:06:53 -0300

    Refatora 2

--- fizbuzz.py
+++ fizbuzz.py
@@ -10,10 +10,10 @@ Regras do Fizbuzz
 
 def robot(pos):
     if pos == 4:
-        return str(4)
+        return str(pos)
     if pos == 2:
-        return str(2)
-    return str(1)
+        return str(pos)
+    return str(pos)
 
 if __name__ == '__main__':
     assert robot(1) == '1'
